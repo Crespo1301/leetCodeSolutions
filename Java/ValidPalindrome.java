d329185cff4602c7237110e7c861092f76cb1308
Crespo3 <ccrespo3@dons.usfca.edu>
2026-01-02 15:00:38 -0500

    Valid Palindrome added.

---
+++ Java/ValidPalindrome.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class ValidPalindrome
+{
+    public boolean isPalindrome(String s) 
+    {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        for(int i = 0; i < s.length() / 2; i++)
+        {
+            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
