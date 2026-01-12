c8aebe6c27f5b86a897e5bfe7e8edba4be01b337
Crespo3 <ccrespo3@dons.usfca.edu>
2026-01-12 12:20:14 -0500

    stack.isEmpty() resolved that.

--- Java/ValidParentheses.java
+++ Java/ValidParentheses.java
@@ -5,10 +5,6 @@ public class ValidParentheses
     public boolean isValid(String s) 
     {
         Stack<Character> stack = new Stack<>();
-        if(s.length() == 1) 
-        {
-            return false;
-        }
         for (char c : s.toCharArray()) 
         {
             if (c == '(' || c == '[' || c == '{') 
